c133f4856029a09f76b0e4d024d704da1d60e1a5
snramji <60272573+snramji@users.noreply.github.com>
2020-04-24 20:45:17 -0600

    incremental progress on 130b/c dom.js index.js

--- 03-objects/src/scripts/DOM.js
+++ 03-objects/src/scripts/DOM.js
@@ -1,31 +1,61 @@
 const DOMfunctions = {
-
-    // let cardNum = 1
-    
+   
     buildNewCard(div, node, text) {
         
-        // console.log("This is a new card")
-        const cardSection = document.getElementById("idAcctCardSection");
+        // const cardSection = document.getElementById("idAcctCardSection");
+
+        // const div = document.createElement('div');
         
         div.classList.add("clAcctCards");
     
         const cardTitle = document.createElement("strong");
-        cardTitle.textContent = "Account Name";
+        cardTitle.textContent = text.name;
         cardTitle.classList.add("text-center");
         div.appendChild(cardTitle);
 
         const breakLine1 = document.createElement("br");
         div.appendChild(breakLine1);
 
+        const labelCurrBal = document.createElement("strong");
+        labelCurrBal.textContent = "Current Balance:";
+        labelCurrBal.classList.add("text-center");
+        div.appendChild(labelCurrBal);
+        
         const breakLine2 = document.createElement("br");
         div.appendChild(breakLine2);
+
+        let currentBalance = document.createElement("P");
+        currentBalance.textContent = "$" + text.balance;
+        currentBalance.classList.add("text-center");
+        div.appendChild(currentBalance);
+
+        const breakLine3 = document.createElement("br");
+        div.appendChild(breakLine3);
 
         const removeAcctButton = document.createElement('button');
         removeAcctButton.append(document.createTextNode("Remove Account"));
         removeAcctButton.classList.add("button");
         div.appendChild(removeAcctButton);
 
-    };
+    },
+
+    acctsDropDown(text) {
+        const options = document.createElement('OPTION');
+        options.setAttribute('id', text.name);
+        options.appendChild(document.createTextNode(text.name));
+        return options;
+    },
+
+    attachToDD(node, text) {
+        const options = DOMfunctions.acctsDropDown(text);
+        console.log(node);
+        node.appendChild(options, node);
+    },
+
+    deleteAcctCard(node) {
+        node.remove();
+    },
+
 
 };
 
--- 03-objects/src/scripts/index.js
+++ 03-objects/src/scripts/index.js
@@ -1,7 +1,8 @@
 import DOMfunctions from './DOM.js';
 import ooStuff from './account.js';
 
-
+let acctControls = new ooStuff.acctControls();
+let i = 0;
 
 document.body.addEventListener("click", e => {
 
@@ -9,17 +10,27 @@ document.body.addEventListener("click", e => {
 
         if (e.target.textContent === "Add Account") {
 
-            // console.log("Add Account Button Clicked");
+            console.log("Add Account Button Clicked");
+
+            if (acctName.value.length > 0 && initDeposit.value.length > 0) {
+                AcctControls.addAcct(acctName.value, Number(initDeposit.value));
+                DOMfunctions.buildNewCard(idAcctCards, (AcctControls.acctArray[i]));
+                DOMfunctions.
+            }
+
+
+
+            // const cardSection = document.getElementById("idAcctCards");
             
-            let div = document.createElement("div");
-            DOMfunctions.buildNewCard(div);
-            cardSection.appendChild(div);
+            // let div = document.createElement("div");
+            // DOMfunctions.buildNewCard(div);
+            // cardSection.appendChild(div);
             
-            if 
+            
 
         } else if (e.target.textContent === "Deposit") {
 
-            // console.log("Deposit Button Clicked");
+            console.log("Deposit Button Clicked");
 
             // let div = document.createElement("div");
             // buildNewCard(div);
@@ -27,7 +38,7 @@ document.body.addEventListener("click", e => {
 
         } else if (e.target.textContent === "Withdraw") {
 
-            // console.log("Add After Button Clicked");
+            console.log("Withdraw Button Clicked");
 
             // let div = document.createElement("div");
             // buildNewCard(div);
@@ -35,15 +46,15 @@ document.body.addEventListener("click", e => {
             
         } else if (e.target.textContent === "Remove Account") {
 
-            // console.log("Remove Account Button Clicked");
+            console.log("Remove Account Button Clicked");
 
-            let div = document.getElementById("idCardSection");
+            // let div = document.getElementById("idCardSection");
             
-            div.removeChild(e.target.parentElement);
+            // div.removeChild(e.target.parentElement);
             
         } else if (e.target.textContent === "Lowest Account Balance") {
 
-            // console.log("Lowest Account Balance Button Clicked");
+            console.log("Lowest Account Balance Button Clicked");
 
             // let div = document.getElementById("idCardSection");
             
@@ -51,7 +62,7 @@ document.body.addEventListener("click", e => {
            
         } else if (e.target.textContent === "Highest Account Balance") {
 
-            // console.log("Highest Account Button Clicked");
+            console.log("Highest Account Button Clicked");
 
             // let div = document.getElementById("idCardSection");
             
@@ -59,7 +70,7 @@ document.body.addEventListener("click", e => {
           
         } else if (e.target.textContent === "Total Account Balance") {
 
-            // console.log("Total Account Balance Button Clicked");
+            console.log("Total Account Balance Button Clicked");
 
             // let div = document.getElementById("idCardSection");
             
